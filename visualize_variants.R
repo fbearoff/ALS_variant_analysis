4fde0bc00aae0f0bcd2acc1430ebf4af708629a8
Frank Bearoff <fbearoff@users.noreply.github.com>
2017-11-13 11:46:21 -0500

    play with masking criteria

--- visualize_variants.R
+++ visualize_variants.R
@@ -2,15 +2,15 @@ library(vcfR)
 library(reshape2)
 library(ggplot2)
 
-vcf <- read.vcfR("SS17.Chr17.recode.vcf")
+vcf <- read.vcfR("BB.Chr17.recode.vcf")
 dna <- ape::read.dna("/home/frank/GRCm38_construct/Mus_musculus.GRCm38.dna.chromosome.17.fa", format = "fasta")
 gff <- read.table("/home/frank/GRCm38_construct/Mus_musculus.GRCm38.89.gff3.gz", sep="\t", quote="")
 gff2 <- gff[grep("17", gff[,1]),]
 
+#depth violin plot
 dp <- extract.gt(vcf, element = 'DP', as.numeric = TRUE)
 dpf <- melt(dp, varnames=c('Index', 'Sample'), value.name="Depth", na.rm=TRUE)
 dpf <- dpf[ dpf$Depth > 0,]
-
 p <- ggplot(dpf, aes(x=Sample, y=Depth)) +
   geom_violin(fill='#C0C0C0', adjust=1.0,
             scale = 'count', trim=TRUE)
@@ -26,8 +26,12 @@ p
 
 heatmap.bp(dp[501:1500,], rlabels = FALSE)
 
-chrom <- create.chromR(name="Chr17", vcf = vcf, seq = dna, ann = gff2)
+chrom <- create.chromR(name="BB", vcf = vcf, seq = dna, ann = gff2)
 chrom <- proc.chromR(chrom)
-chromoqc(chrom, xlim=c(34956436, 34959238), dp.alpha=20) #xlim determines interval graphed
-
+chromoqc(chrom, dp.alpha=20) 
+#chromoqc(chrom, xlim=c(34956436, 34959238), dp.alpha=20) #xlim determines interval graphed
 plot(chrom)
+
+#setup some baseline filters
+chrom_mask <- masker(chrom, min_QUAL=30, min_DP=3, max_DP=100, min_MQ=59, max_MQ=61)
+plot(chrom_mask)
